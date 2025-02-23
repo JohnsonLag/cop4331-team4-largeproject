d66d5e3fc2eb45e9bf7b57e8283342b7a1600ca9
dooljonat <jonathandool75@gmail.com>
2025-02-22 23:09:05 -0500

    Got basic login page working. Note: Had to change some code from the example code because we're using react-router-dom v6 instead of v5 which changes the routing mechanics of React

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+function Login() {
+    const [message,setMessage] = useState('');
+    const [loginName,setLoginName] = React.useState('');
+    const [loginPassword,setPassword] = React.useState('');
+
+    function handleSetLoginName( e: any ) : void
+    {
+        setLoginName( e.target.value );
+    }
+    
+    function handleSetPassword( e: any ) : void
+    {
+        setPassword( e.target.value );
+    }
+
+    function doLogin(event:any) : void {
+        event.preventDefault();
+        alert('doIt()' + loginName + ' ' + loginPassword);
+    }
+
+    return (
+        <div id="loginDiv">
+            <span id="inner-title">PLEASE LOG IN</span><br></br>
+            <input type="text" id="loginName" placeholder="Username" onChange={handleSetLoginName} />
+            <input type="password" id="loginPassword" placeholder="Password" onChange={handleSetPassword} />
+            <input type="submit" id="loginButton" className="buttons" value = "Do It"
+            onClick={doLogin} />
+            <span id="loginResult">{message}</span>
+        </div>
+    );
+};
+
+export default Login;
