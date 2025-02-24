556579b13ef846e6217e83eb29958cb5e99342d0
dooljonat <jonathandool75@gmail.com>
2025-02-23 20:28:57 -0500

    Connected to MongoDB, added input persistence

--- frontend/src/components/LoggedInName.tsx
+++ frontend/src/components/LoggedInName.tsx
@@ -1,18 +1,30 @@
 function LoggedInName()
 {
-    var user={}
+    var _ud = localStorage.getItem('user_data');
+    // Null check for _ud
+    if (!_ud) {
+        // throw new Error("User data in localStorage is NULL");
+        console.log("Error: UserData in localStorage is NULL");
+        window.location.href = '/login';
+        return null;
+    }
 
-    function doLogout(event:any) : void
-    {
+    var ud = JSON.parse(_ud);
+    var userId = ud.id;
+    var firstName = ud.firstName;
+    var lastName = ud.lastName;
+
+    // Logout handler
+    const doLogout = (event: React.MouseEvent<HTMLButtonElement>): void => {
         event.preventDefault();
-        alert('doLogout');
+        localStorage.removeItem("user_data");
+        window.location.href = '/';
     };
-    
+
     return(
         <div id="loggedInDiv">
-        <span id="userName">Logged In As John Doe </span><br />
-        <button type="button" id="logoutButton" className="buttons"
-        onClick={doLogout}> Log Out </button>
+            <span id="userName">Logged In As {firstName} {lastName}</span><br />
+            <button type="button" id="logoutButton" className="buttons" onClick={doLogout}> Log Out </button>
         </div>
     );
 };
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const config = require('./config');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -6,141 +8,58 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-var cardList =
-[
-'Roy Campanella',
-'Paul Molitor',
-'Tony Gwynn',
-'Dennis Eckersley',
-'Reggie Jackson',
-'Gaylord Perry',
-'Buck Leonard',
-'Rollie Fingers',
-'Charlie Gehringer',
-'Wade Boggs',
-'Carl Hubbell',
-'Dave Winfield',
-'Jackie Robinson',
-'Ken Griffey, Jr.',
-'Al Simmons',
-'Chuck Klein',
-'Mel Ott',
-'Mark McGwire',
-'Nolan Ryan',
-'Ralph Kiner',
-'Yogi Berra',
-'Goose Goslin',
-'Greg Maddux',
-'Frankie Frisch',
-'Ernie Banks',
-'Ozzie Smith',
-'Hank Greenberg',
-'Kirby Puckett',
-'Bob Feller',
-'Dizzy Dean',
-'Joe Jackson',
-'Sam Crawford',
-'Barry Bonds',
-'Duke Snider',
-'George Sisler',
-'Ed Walsh',
-'Tom Seaver',
-'Willie Stargell',
-'Bob Gibson',
-'Brooks Robinson',
-'Steve Carlton',
-'Joe Medwick',
-'Nap Lajoie',
-'Cal Ripken, Jr.',
-'Mike Schmidt',
-'Eddie Murray',
-'Tris Speaker',
-'Al Kaline',
-'Sandy Koufax',
-'Willie Keeler',
-'Pete Rose',
-'Robin Roberts',
-'Eddie Collins',
-'Lefty Gomez',
-'Lefty Grove',
-'Carl Yastrzemski',
-'Frank Robinson',
-'Juan Marichal',
-'Warren Spahn',
-'Pie Traynor',
-'Roberto Clemente',
-'Harmon Killebrew',
-'Satchel Paige',
-'Eddie Plank',
-'Josh Gibson',
-'Oscar Charleston',
-'Mickey Mantle',
-'Cool Papa Bell',
-'Johnny Bench',
-'Mickey Cochrane',
-'Jimmie Foxx',
-'Jim Palmer',
-'Cy Young',
-'Eddie Mathews',
-'Honus Wagner',
-'Paul Waner',
-'Grover Alexander',
-'Rod Carew',
-'Joe DiMaggio',
-'Joe Morgan',
-'Stan Musial',
-'Bill Terry',
-'Rogers Hornsby',
-'Lou Brock',
-'Ted Williams',
-'Bill Dickey',
-'Christy Mathewson',
-'Willie McCovey',
-'Lou Gehrig',
-'George Brett',
-'Hank Aaron',
-'Harry Heilmann',
-'Walter Johnson',
-'Roger Clemens',
-'Ty Cobb',
-'Whitey Ford',
-'Willie Mays',
-'Rickey Henderson',
-'Babe Ruth'
-];
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient(config.mongoURL);
+client.connect();
+
 
 app.post('/api/addcard', async (req, res, next) =>
 {
     // incoming: userId, color
     // outgoing: error
-    var error = '';
     const { userId, card } = req.body;
-    // TEMP FOR LOCAL TESTING.
+    const newCard = {Card:card,UserId:userId};
+    var error = '';
+
+    try
+    {
+        const db = client.db();
+        const result = db.collection('Cards').insertOne(newCard);
+    }
+    catch(e)
+    {
+        error = e.toString();
+    }
+
     cardList.push( card );
     var ret = { error: error };
-    res.status(200).json(ret);
+    res.status(200).tsxon(ret);
 });
 
 app.post('/api/login', async (req, res, next) =>
 {
     // incoming: login, password
     // outgoing: id, firstName, lastName, error
+
     var error = '';
+
     const { login, password } = req.body;
+
+    const db = client.db();
+    const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
+
     var id = -1;
     var fn = '';
     var ln = '';
-    if( login.toLowerCase() == 'rickl' && password == 'COP4331' )
+    
+    if( results.length > 0 )
     {
-        id = 1;
-        fn = 'Rick';
-        ln = 'Leinecker';
+        id = results[0].UserId;
+        fn = results[0].FirstName;
+        ln = results[0].LastName;
     }
-    else
-    {
-        error = 'Invalid user name/password';
-    }
-    var ret = { id:id, firstName:fn, lastName:ln, error:error};
+
+    var ret = { id:id, firstName:fn, lastName:ln, error:''};
     res.status(200).json(ret);
 });
 
@@ -150,18 +69,18 @@ app.post('/api/searchcards', async (req, res, next) =>
     // outgoing: results[], error
     var error = '';
     const { userId, search } = req.body;
-    var _search = search.toLowerCase().trim();
+    var _search = search.trim();
+    const db = client.db();
+    const results = await db.collection('Cards').find({"Card":{$regex:_search+'.*', $options:'r'}}).toArray();
+
     var _ret = [];
-    for( var i=0; i<cardList.length; i++ )
+    for( var i=0; i<results.length; i++ )
     {
-        var lowerFromList = cardList[i].toLocaleLowerCase();
-        if( lowerFromList.indexOf( _search ) >= 0 )
-        {
-            _ret.push( cardList[i] );
-        }
+        _ret.push( results[i].Card );
     }
-    var ret = {results:_ret, error:''};
-    res.status(200).json(ret);
+
+    var ret = {results:_ret, error:error};
+    res.status(200).tsxon(ret);
 });
 
 app.use((req, res, next) =>
