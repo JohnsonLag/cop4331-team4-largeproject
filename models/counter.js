5dc5522871a9334b03453e5cfe2d2ee46dd72491
dooljonat <jonathandool75@gmail.com>
2025-03-15 08:49:26 -0400

    Changed redundant naming of counter

--- models/counter.js
+++ models/counter.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const UserIdCounterSchema = new mongoose.Schema({
+const CounterSchema = new mongoose.Schema({
   _id: { type: String, required: true }, // Identifier for the counter (e.g., "userId")
   sequence_value: { type: Number, default: 0 }, // Current value of the counter
 });
 
-module.exports = userIdCounter = mongoose.model("UserIdCounter", UserIdCounterSchema, "UserIdCounter");
+module.exports = counter = mongoose.model("Counter", CounterSchema, "Counter");
--- userIdGenerator.js
+++ userIdGenerator.js
@@ -1,7 +1,7 @@
-const UserIdCounter = require('./models/userIdCounter');
+const Counter = require('./models/counter');
 
 async function getNextUserId() {
-  const counter = await UserIdCounter.findOneAndUpdate(
+  const counter = await Counter.findOneAndUpdate(
     { _id: 'userId' }, // Identifier for the counter
     { $inc: { sequence_value: 1 } }, // Increment the sequence_value by 1
     { new: true, upsert: true } // Create the document if it doesn't exist
