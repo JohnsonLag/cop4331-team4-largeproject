c2c728d3862e75b8ab4433218e9b885e54002103
dooljonat <jonathandool75@gmail.com>
2025-03-19 18:31:53 -0400

    Removed unused imports

--- frontend/src/pages/NotePage.tsx
+++ frontend/src/pages/NotePage.tsx
@@ -1,6 +1,4 @@
 import MenuBar from '../components/MenuBar';
-import PageTitle from '../components/PageTitle';
-import LoggedInName from '../components/LoggedInName';
 import AddNote from '../components/TEMP_AddNote';
 import SearchNotes from '../components/TEMP_SearchNotes';
 
