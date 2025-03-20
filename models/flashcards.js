db30e9ad7af4a349370f816bf3599fe74e7ac7a9
dooljonat <jonathandool75@gmail.com>
2025-03-19 22:21:51 -0400

    Defined flashcards model

---
+++ models/flashcards.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+//Create Schema
+const FlashCardSchema = new Schema({
+    UserId: {
+        type: Number,
+        required: true
+    },
+    DeckId: {
+        type: Number,
+        required: true
+    },
+    Question: {
+        type: String,
+        required: true
+    },
+    Answer: {
+        type: [String],
+        required: true
+    },
+});
+module.exports = notes = mongoose.model("FlashCards", FlashCardSchema, "FlashCards");
