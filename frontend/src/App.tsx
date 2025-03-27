1cbbb1009a1b0e1673e85be6d41f3b3ff8a17dfb
dooljonat <jonathandool75@gmail.com>
2025-03-27 15:02:25 -0400

    Started on flashcard review page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import SingleNotePage from './pages/Notes/SingleNotePage';
 import DevPage from './pages/Notes/DevPage';
 import FlashCardDecksPage from './pages/FlashCardDecks/FlashCardDecksPage';
 import FlashCardDeckPage from './pages/FlashCardDecks/FlashCardDeckPage';
+import ReviewFlashCardsPage from './pages/FlashCards/ReviewFlashCardsPage';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
               <Route path="/notes/:id" element={<SingleNotePage />} />
               <Route path="/decks" element={<FlashCardDecksPage />} />
               <Route path="/decks/:deckId" element={<FlashCardDeckPage />} />
+              <Route path="/decks/:deckId/review" element={<ReviewFlashCardsPage/>} />
               <Route path="/dev" element={<DevPage />} />
               {/* Default redirect if no route matches */}
               <Route path="*" element={<Navigate replace to="/" />} />
---
+++ frontend/src/pages/FlashCards/ReviewFlashCardsPage.tsx
@@ -0,0 +1,14 @@
+import MenuBar from '../../components/MenuBar';
+import ReviewFlashCards from '../../components/FlashCards/ReviewFlashCards';
+
+const ReviewFlashCardsPage = () =>
+{
+    return(
+        <div>
+        <MenuBar />
+        <ReviewFlashCards />
+        </div>
+    );
+}
+
+export default ReviewFlashCardsPage;
