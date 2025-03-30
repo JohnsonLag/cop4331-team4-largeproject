0bca7082050518eff00e4199eb60de99d3c43db0
Cristian McGee <143134870+nizswan@users.noreply.github.com>
2025-03-30 18:08:49 -0400

    Update ResetPasswordPage.tsx
    added menu bar

--- frontend/src/pages/ResetPasswordPage.tsx
+++ frontend/src/pages/ResetPasswordPage.tsx
@@ -1,9 +1,11 @@
+import MenuBar from '../components/MenuBar.tsx';
 import ResetPassword from '../components/User/ResetPassword.tsx';
 
 const ResetPasswordPage = () =>
 {
     return(
         <div>
+        <MenuBar />
         <ResetPassword />
         </div>
     );
