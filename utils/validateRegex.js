a8b318058623348864a07006dcf2898f9fb78d9d
dooljonat <jonathandool75@gmail.com>
2025-04-07 14:45:06 -0400

    Added password validation

--- utils/validateRegex.js
+++ utils/validateRegex.js
@@ -4,3 +4,9 @@ const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 export function isValidEmail(email) {
     return emailRegex.test(email);
 }
+
+const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+export function isValidPassword(password) {
+    return passwordRegex.test(password);
+}
